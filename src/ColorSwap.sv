d04ba32777162cbb97253217810e26c22a56bf8d
Ilya Pikin <hypnotiod@gmail.com>
2020-07-31 12:23:00 +0300

    Fixed color swapper

--- src/ColorSwap.sv
+++ src/ColorSwap.sv
@@ -12,11 +12,11 @@ module ColorSwap
 
 always @(*) begin
 	case (swapIN)
-		2'b001 : dataOUT[23:0] = {dataIN[23:16], dataIN[7:0],   dataIN[15:8]};
-		2'b010 : dataOUT[23:0] = {dataIN[7:0],   dataIN[15:8],  dataIN[23:16]};
-		2'b011 : dataOUT[23:0] = {dataIN[7:0],   dataIN[23:16], dataIN[15:8]};
-		2'b100 : dataOUT[23:0] = {dataIN[15:8],  dataIN[7:0],   dataIN[23:16]};
-		2'b101 : dataOUT[23:0] = {dataIN[15:8],  dataIN[23:16], dataIN[7:0]};
+		3'd1 : dataOUT[23:0] = {dataIN[23:16], dataIN[7:0],   dataIN[15:8]};
+		3'd2 : dataOUT[23:0] = {dataIN[7:0],   dataIN[15:8],  dataIN[23:16]};
+		3'd3 : dataOUT[23:0] = {dataIN[7:0],   dataIN[23:16], dataIN[15:8]};
+		3'd4 : dataOUT[23:0] = {dataIN[15:8],  dataIN[7:0],   dataIN[23:16]};
+		3'd5 : dataOUT[23:0] = {dataIN[15:8],  dataIN[23:16], dataIN[7:0]};
 		default : dataOUT[23:0] = {dataIN[23:16], dataIN[15:8],  dataIN[7:0]};
 	endcase
 end
